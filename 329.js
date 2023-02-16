86380e34df05b8c28c43fc8864cd4dfb6e99f314
Wilbur <85022787+Pupper0n1@users.noreply.github.com>
2023-02-16 14:43:59 -0700

    Added copy readonly text-boxes

--- 329.js
+++ 329.js
@@ -1,8 +1,29 @@
-function hover(img)
-{
+// Changes the trashcan image from closed trash can image to the open trash can image 
+function hover(img) {
  img.src = "images/open-trash-can.png";
 }
 
-function hoverOut(img){
+
+// Changes the trashcan image from open trash can image to the closed trashcan image
+function hoverOut(img) {
     img.src = "images/closed-trash-can.png"
    }
+
+
+// Button that copys to clipboard
+// Used to copy the username/password in gallery.html
+function copy() {
+    var copyText = document.getElementById("copyText");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("COPIED");
+}
+
+
